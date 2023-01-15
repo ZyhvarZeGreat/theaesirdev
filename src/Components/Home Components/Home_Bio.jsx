f11285acc4e2d0aec9801fa10c4632e2be5db22b
TheAesirDev <boludedaniel@gmail.com>
2023-01-15 14:11:23 +0100

    tweak

--- src/Components/Home Components/Home_Bio.jsx
+++ src/Components/Home Components/Home_Bio.jsx
@@ -1,6 +1,5 @@
 import React, { useRef, useEffect } from "react";
 import "./Home_Bio.css";
-import bio_image from "../../assets/main_bio.jpg";
 import { ScrollTrigger } from "gsap/src/all";
 import scrollElement from "../../Animations/scrollElement";
 import { gsap, TweenMax, TimelineLite, Power2 } from "gsap/gsap-core";
@@ -108,7 +107,7 @@ const Home_Bio = () => {
         md={6}
         ref={(el) => (container = el)}
       >
-        <img ref={(el) => (image = el)} src={bio_image} alt="bio image" />
+        <img ref={(el) => (image = el)} src={"https://tabodozo.sirv.com/Portfolio%20Assets/main_bio.jpg"} alt="bio image" />
       </Grid>
     </Grid>
   );
